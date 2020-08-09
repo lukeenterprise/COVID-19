f537714111cf9d2ab8894609a69138a2a4544000
Plug in Git <gitbot@plugingit.com>
2020-08-09 10:01:59 +0000

    Merge current Shopify theme changes

---
+++ assets/clever-adwords-conversion.js
@@ -0,0 +1,1 @@
+new Image(1,1).src = 'https://www.googleadservices.com/pagead/conversion/751900699/?currency='+Shopify.checkout.currency'/?value='+Shopify.checkout.total_price+'&label=OLkACJGkr9oBEJuwxOYC&guid=ON&oid='+Shopify.checkout.id+'&script=0';
